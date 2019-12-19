ce2b1955765f09ae665b7c7d01466b3d4ffaa183
Nahiyan Kamal <noyanayihan@gmail.com>
2019-12-19 17:30:00 +0100

    styled components start

---
+++ src/styles/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const CardWrapper = styled.div`
+  height: 50px;
+  width: 170px;
+  margin: 0 auto;
+  font-size: xx-large;
+  text-align: center;
+`;
+
+export const FormControlWrapper = styled.div`
+  display: flex;
+  width: 160px;
+  margin: 0 auto;
+  align-content: center;
+  justify-content: center;
+`;
