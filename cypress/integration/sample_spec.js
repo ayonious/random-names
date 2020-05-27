0ac20f5d89ad71076aa4996c0a522c0cde939155
Nahiyan Kamal <noyanayihan@gmail.com>
2020-05-27 07:22:42 +0200

    auto generated message

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,17 +1,19 @@
 describe('Integration Tests', () => {
-  it('initial page should have no name shown', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:8080');
-    cy.get('.MuiCard-root').invoke('text').should('contain', '');
   });
 
-  it('After clicking on shuffle, should see a name', () => {
-    cy.visit('http://localhost:8080');
+  it('initial page should have => 1.no name shown, 2.shuffle button', () => {
+    cy.get('.MuiCard-root').invoke('text').should('contain', '');
+    cy.contains('Shuffle');
+  });
+
+  it('After clicking on shuffle => should see a name', () => {
     cy.contains('Shuffle').click();
     cy.get('.MuiCard-root').invoke('text').should('match', /\w+/); // word with at least one char
   });
 
-  it('Select other country => click on shuffle, should see a name', () => {
-    cy.visit('http://localhost:8080');
+  it('Select other country => click on shuffle => should see a name', () => {
     cy.contains('Germany').click();
     cy.contains('Italy').click();
 
