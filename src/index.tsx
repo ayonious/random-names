15527ca67b0411ef31b4d21b400bfb9f6f079601
Nahiyan Kamal <noyanayihan@gmail.com>
2020-06-11 01:29:35 +0200

    auto generated message

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 
 const rootElement = document.getElementById('root');
 render(<Dashboard />, rootElement);
