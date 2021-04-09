521b49e769282280b6cea71fb1c4a3366bea05b2
Nahiyan Kamal <noyanayihan@gmail.com>
2021-04-10 00:13:31 +0200

    fix(deps): update deps

--- cypress/integration/visual-test.js
+++ cypress/integration/visual-test.js
@@ -1,11 +1,6 @@
 describe('Visual Testing', () => {
-  const SCREENS = [375, 768, 1280];
-
+  
   it('Responsiveness Testing', () => {
     cy.visit('http://localhost:8080');
-
-    cy.percySnapshot('Home Page is Responsive', {
-      widths: SCREENS,
-    });
   });
 });
